6e6f7cffdef937206fb2298b1a54d2d458c91197
alphaseneca <alphatheseneca@protonmail.com>
2024-06-18 23:14:04 +0545

    completed the styling of the inputpage

--- bmicalculator/lib/input_page.dart
+++ bmicalculator/lib/input_page.dart
@@ -238,9 +238,15 @@ class _InputPageState extends State<InputPage> {
             child: Container(
               color: bottomContainerColour,
               margin: EdgeInsets.only(top: 10.0),
+              padding: EdgeInsets.only(bottom: 20.0),
               width: double.infinity,
               height: bottomContainerHeight,
-              child: Text('CALCULATE'),
+              child: Center(
+                child: Text(
+                  'CALCULATE',
+                  style: largeButtonTextStyle,
+                ),
+              ),
             ),
           ),
         ],
